7156a3202902b1dbbffca1c7ccc30f8421f18bc1
CacucoH <d.yashin@innopolis.univeristy>
2025-07-29 22:01:50 +0300

    Major improvements: + Added DB interactions + Introduced interactive bot form better UI/UX + Architectural improvements

--- src/events/handlers/message_handler.py
+++ src/events/handlers/message_handler.py
@@ -1,52 +1,46 @@
 import os
 from telethon import TelegramClient, events
+from telethon.events.newmessage import NewMessage
 from telethon.tl.patched import Message
 from telethon.tl.types import User 
 
-from db.operations import AsyncSessionLocal
+from events.user_interaction.gui_settings import main_menu_buttons
+from db.operations import add_msg, get_user_settings
 from db.schema import Message
-from datetime import datetime
 
 
 async def handle_new_message(event: events.NewMessage.Event):
     message: Message = event.message
     user_id = message.sender_id
 
-    if not event.is_private:
+    # In case bot is deactivated
+    settings = await get_user_settings()
+    if settings.bot_active is False:
         return
 
+    # Skip msg if there is no sender or if the sender is a bot
+    user: User = message.sender
+    if not user or user.bot:
+        return
+
+    # Get minimal userinfo
+    sender_id = str(message.sender_id)
+    if user:
+        sender_id = user.username
+
+    await add_msg(
+        tg_msg_id=message.id,
+        chat_id=message.chat_id,
+        sender_id=sender_id,
+        content=message.text or "<No text content>"
+    )
+    
     print(f"New message from user ID: {user_id} {message.text}")
-    async with AsyncSessionLocal() as session:
-        # Get user info
-        # user = await session.get(User, user_id)
-        
-        # is_filtered = await session.get(UserSettings)
-
-        # if not user:
-        #     user = User(id=user_id, username=event.sender.username)
-        #     session.add(user)
-        #     await session.commit()
-
-        user: User = message.sender
-
-        # Check if user is whitelisted
-        # if user.status and user.status.is_whitelisted:
-            # Save message to database then
 
 
-        sender_id = str(message.sender_id)
-        if user:
-            sender_id = user.username
-
-        message = Message(
-            telegram_message_id=str(message.id),
-            chat_id=str(message.chat_id),
-            sender_id=sender_id,
-            content=message.raw_text
-        )
-        session.add(message)
-        await session.commit()
-        print(f"Message from {sender_id} saved to the database.")
-
-        # else:
-        #     print(f"User {user.username} is not in whitelist, message not saved.")
+async def handle_start_message(event: NewMessage.Event, edit: bool = False):
+    if edit:
+        await event.edit("Выберете действие:", buttons=await main_menu_buttons())
+        return
+    
+    await event.reply("Выберете действие:", buttons=await main_menu_buttons())
